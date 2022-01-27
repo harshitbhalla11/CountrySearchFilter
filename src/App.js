61822a344de3de92cc3dc79450c670ef4944692e
harshitbhalla <harshit.bhalla@hcl.com>
2022-01-27 14:26:10 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import CountryCard from "./CountryCard"
+import "./App.css";
+ import { FaSearchLocation } from 'react-icons/fa';
 function App() {
+  let [Data, setData] = useState([]);
+  let [Search, setSearch] = useState([]);
+
+  useEffect(() => {
+    async function APIcalling() {
+      let response = await fetch("https://restcountries.com/v3.1/all");
+      let data = await response.json();
+      setData(data);
+    }
+    APIcalling();
+  }, []);
+function inputValue(e){
+  let input= e.target.value.toLowerCase();
+  setSearch(input);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+     
+    <label className="searchLabel">Search Country</label>
+    <FaSearchLocation /> <input type="text" className="searchFiled" onChange={(e)=>inputValue(e)}/>
+    <div className="card-container">
+        {Data.filter((country)=>{return country.name.common.toLowerCase().indexOf(Search)!== -1}).map((users) => {
+          return (
+          <CountryCard
+          keyValue={users.ccn3}
+          key={users.ccn3}
+              name={users.name.common}
+              flag={users.flags.svg}
+              population={users.population}
+          
+          />
+          )
+        })}
+        </div>
+      
+        </div>
   );
 }
 
---
+++ src/CountryCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Card, Row,Col} from "react-bootstrap";
+import "./CountryCard.css"
+function CountryCard(props) {
+
+
+    return(
+      <Row xs={1} md={2} className="g-4 ">
+     
+        <Col xs={1} md={2} lg={2}>
+          <Card className="card" key={props.keyValue}>
+        <Card.Img className="countryImage" variant="top" src={props.flag} />
+        <Card.Body>
+          <Card.Title className="title">{props.name}</Card.Title>
+          <Card.Text className="text">
+            Population = {props.population}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+        </Col>
+    
+    </Row>
+  )
+}
+export default CountryCard;
